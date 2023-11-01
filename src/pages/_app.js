46d7c06de44aa041e5505ff71c87f996141710d0
tinyphoniex <royalguy1031@gmail.com>
2023-11-01 13:33:36 +0900

    vercel test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,7 @@
-import Layout from "@/components/layout/layout";
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Component {...pageProps} />
   )
 }

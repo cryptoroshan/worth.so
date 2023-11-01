511de8db18afc8144f9676f8ebf5b09ee69108f2
tinyphoniex <royalguy1031@gmail.com>
2023-11-01 14:04:52 +0900

    vercel test

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -82,34 +82,6 @@ const AddSaaSModal = ({ modalOpen, handleClose }) => {
 
   return (
     <Dialog open={open} size="xs" handler={handleCancel}>
-      <div className="flex items-center justify-between">
-        <DialogHeader className="flex flex-col items-start">
-          {" "}
-          <Typography className="mb-1" variant="h4">
-            Add new SaaS product
-          </Typography>
-        </DialogHeader>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="mr-3 h-5 w-5 cursor-pointer"
-          onClick={handleCancel}
-        >
-          <path
-            fillRule="evenodd"
-            d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </div>
-      <DialogBody className="overflow-y-auto max-h-[70vh]">
-      </DialogBody>
-      <DialogFooter className="space-x-2">
-        <Button variant="text" color="gray" onClick={handleCancel}>
-          cancel
-        </Button>
-      </DialogFooter>
     </Dialog>
   );
 }
